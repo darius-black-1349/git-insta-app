d83a8564fda880ababf34bd50dff0e026efde0b9
darius <darius134966@gmail.com>
2021-12-27 08:26:38 +0330

    fixed responsive sign in page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,45 +6,53 @@ function signIn({ providers }) {
   const router = useRouter()
 
   return (
-    <div
-      style={{
-        background: 'url("/images/bg_login.svg") no-repeat',
-        backgroundSize: 'cover',
-      }}
-      className="flex"
-    >
-      <div className="flex flex-col min-h-screen justify-start pt-40 items-center bg-slate-900 px-5 shadow-slate-900 shadow-2xl">
-        <img
-          src="/images/signin.png"
-          alt="insta_icon"
-          className="text-white w-24 h-24 animate-pulse"
-        />
-        <p className="text-lg font-semibold text-white mt-10">
-          Welcome To Instagram
-        </p>
-        <button
-          className="text-white bg-slate-800 rounded-md text-center font-semibold p-2 mt-20"
-          onClick={() => router.replace('/')}
-        >
-          Back To Home
-        </button>
-      </div>
-      <div className="flex flex-col justify-center mx-auto items-center min-h-screen min-w-screen py-2">
-        <img src="/images/insta_txt.png" alt="insta-logo" className="w-80" />
+    <div className="">
+      <div
+        style={{
+          background: 'url("/images/bg_login.svg") no-repeat',
+          backgroundSize: 'cover',
+        }}
+        className="flex flex-col md:flex-row md:overflow-auto overflow-hidden"
+      >
+        <div className="flex flex-row md:flex-col min-h-0 md:min-h-screen justify-between md:justify-start pt-2 pb-2 md:pt-40 md:pb-0 items-center bg-slate-900 px-5 shadow-slate-900 shadow-2xl">
+          <img
+            src="/images/signin.png"
+            alt="insta_icon"
+            className="text-white w-14 md:w-24 h-14 md:h-24 animate-pulse"
+          />
+          <p className="md:text-lg text-sm font-semibold text-white mt-0 md:mt-10">
+            Welcome To Instagram
+          </p>
+          <button
+            className="text-white bg-slate-800 rounded-md text-center font-semibold p-2 mt-0 md:mt-20 hidden md:block"
+            onClick={() => router.replace('/')}
+          >
+            Back To Home
+          </button>
+        </div>
+        <div className="flex flex-col justify-center mx-auto items-center min-h-screen min-w-screen py-2">
+          <img src="/images/insta_txt.png" alt="insta-logo" className="w-80" />
 
-        <div className="mt-12 mb-40">
-          {Object.values(providers).map((provider) => (
-            <div key={provider.name}>
-              <button
-                className="p-3 bg-red-600 rounded-lg text-white hover:translate-x-5 transition shadow-lg"
-                onClick={() =>
-                  SignInToProvider(provider.id, { callbackUrl: '/' })
-                }
-              >
-                Sign in with {provider.name}
-              </button>
-            </div>
-          ))}
+          <div className="mt-12 mb-40">
+            {Object.values(providers).map((provider) => (
+              <div key={provider.name} className='flex justify-center flex-col'>
+                <button
+                  className="p-3 bg-red-600 rounded-lg text-white hover:translate-x-5 transition shadow-lg"
+                  onClick={() =>
+                    SignInToProvider(provider.id, { callbackUrl: '/' })
+                  }
+                >
+                  Sign in with {provider.name}
+                </button>
+                <button
+                  className="text-white bg-slate-800 rounded-md text-center font-semibold p-2 mt-20 block md:hidden"
+                  onClick={() => router.replace('/')}
+                >
+                  Back To Home
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
